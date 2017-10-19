b382f0f4fe4c1cf0db2609cb8bfa5e042a661dc8
Dishant <dishantpatel27@gmail.com>
2017-10-19 23:01:48 +0000

    Switched tweeter to MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,19 +9,22 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      db.collection('tweets').insert(newTweet);
+     // simulateDelay(() => {
+      //  db.tweets.push(newTweet);
         callback(null, true);
-      });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      db.collection('tweets').find().toArray((err,result)=>{
+        if (err) throw err;
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, result.sort(sortNewestFirst));
+
       });
     }
-
-  };
+  }
 }
+
+    
